5c3df3679be77837839bc338de2e8072e5aae2d5
Ruggero <infiniteproject@gmail.com>
2016-06-21 00:02:22 +0200

    Minor updates

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ ENV DEBIAN_FRONTEND noninteractive
 ENV CONTAINER_NAME nginx
 ENV CONTAINER_PORT 80
 ENV SERVICE_PORT 80
+
 VOLUME /var/lib/tor
 
 RUN apt-get update && \
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,6 @@
 
 echo "HiddenServiceDir /var/lib/tor/$CONTAINER_NAME/" >> /etc/tor/torrc
 echo "HiddenServicePort $SERVICE_PORT $CONTAINER_NAME:$CONTAINER_PORT" >> /etc/tor/torrc
+echo "Log info stdout" >> /etc/tor/torrc
 
-/usr/bin/tor
+/usr/bin/tor -f /etc/tor/torrc
